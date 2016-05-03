a3e5a6ab310518d893020abc189a3378e4d9436c
mattp94 <matthieu.puibaraud@isep.fr>
2016-05-03 18:52:43 +0800

    add Database + spaces indent

--- components/Explorer.js
+++ components/Explorer.js
@@ -9,84 +9,84 @@ var moment = require('moment')
 
 function Explorer() {
 
-	this.browse = function (directories) { // Get folders with metadata sorted by creation date
-		var folders = []
+    this.browse = function (directories) { // Get folders with metadata sorted by creation date
+        var folders = []
 
-		directories.forEach(function (directory) {
-			fs.readdirSync(directory).forEach(function (element) {
-				var stats = fs.statSync(directory + '/' + element)
+        directories.forEach(function (directory) {
+            fs.readdirSync(directory).forEach(function (element) {
+                var stats = fs.statSync(directory + '/' + element)
 
-				if (stats.isDirectory() && element != '@eaDir') // Only folders different to @eaDir
-					folders.push(addTags(addQuality(addSearch(addDate({
-						dirname: directory,
-						basename: element
-					}, stats)))))
-			})
-		})
+                if (stats.isDirectory() && element != '@eaDir') // Only folders different to @eaDir
+                    folders.push(addTags(addQuality(addSearch(addDate({
+                        dirname: directory,
+                        basename: element
+                    }, stats)))))
+            })
+        })
 
-		return sortByDate(folders)
-	}
+        return sortByDate(folders)
+    }
 
-	function addDate(folder, stats) { // Add folder creation date (read or create hidden file)
-		var date
+    function addDate(folder, stats) { // Add folder creation date (read or create hidden file)
+        var date
 
-		try {
-			date = fs.readFileSync(folder.dirname + '/' + folder.basename + '/.date', 'utf8')
-		} catch (error) {
-			date = moment(stats.mtime).format('YYYY-MM-DD HH:mm:ss')
-			fs.writeFileSync(folder.dirname + '/' + folder.basename + '/.date', date)
-		}
+        try {
+            date = fs.readFileSync(folder.dirname + '/' + folder.basename + '/.date', 'utf8')
+        } catch (error) {
+            date = moment(stats.mtime).format('YYYY-MM-DD HH:mm:ss')
+            fs.writeFileSync(folder.dirname + '/' + folder.basename + '/.date', date)
+        }
 
-		folder.date = date
-		
-		return folder
-	}
+        folder.date = date
+        
+        return folder
+    }
 
-	function addSearch(folder) { // Add search as beautified basename
-		folder.search = folder.basename.replace(/0+PV[0-9]* */i, '')
-									   .replace(/\[([^\[\]]+)\]/g, '')
-									   .replace(/{{[^{}]+}}/g, '')
-									   .replace(/\.([0-9]{4}|french|multi|bluray|vostfr|[0-9]{3,4}p|dvdrip)\..*/i, '')
-									   .toLowerCase()
-									   .trim()
+    function addSearch(folder) { // Add search as beautified basename
+        folder.search = folder.basename.replace(/0+PV[0-9]* */i, '')
+                                       .replace(/\[([^\[\]]+)\]/g, '')
+                                       .replace(/{{[^{}]+}}/g, '')
+                                       .replace(/\.([0-9]{4}|french|multi|bluray|vostfr|[0-9]{3,4}p|dvdrip)\..*/i, '')
+                                       .toLowerCase()
+                                       .trim()
 
-		return folder
-	}
+        return folder
+    }
 
-	function addQuality(folder) { // Add movie's quality e.g. [1080p]
-		var quality = /\[([^\[\]]+)\]/.exec(folder.basename)
+    function addQuality(folder) { // Add movie's quality e.g. [1080p]
+        var quality = /\[([^\[\]]+)\]/.exec(folder.basename)
 
-		if (quality !== null)
-			folder.quality = quality[1]
+        if (quality !== null)
+            folder.quality = quality[1]
 
-		return folder
-	}
+        return folder
+    }
 
-	function addTags(folder) { // Add tags as e.g. {{tag1}} {{tag2}} {{tag3}}
-		var tags = folder.basename.match(/{{[^{}]+}}/g)
+    function addTags(folder) { // Add tags as e.g. {{tag1}} {{tag2}} {{tag3}}
+        var tags = folder.basename.match(/{{[^{}]+}}/g)
 
-		if (tags !== null) {
-			folder.tags = []
+        if (tags !== null) {
+            folder.tags = []
 
-			tags.forEach(function (tag) {
-				folder.tags.push(tag.replace(/[{}]/g, ''))
-			})
-		}
+            tags.forEach(function (tag) {
+                folder.tags.push(tag.replace(/[{}]/g, ''))
+            })
+        }
 
-		return folder
-	}
+        return folder
+    }
 
-	function sortByDate(folders) { // Sort folders by creation date
-		return folders.sort(function (a, b) {
-			if (a.date < b.date)
-				return 1
+    function sortByDate(folders) { // Sort folders by creation date
+        return folders.sort(function (a, b) {
+            if (a.date < b.date)
+                return 1
 
-			if (a.date > b.date)
-				return -1
+            if (a.date > b.date)
+                return -1
 
-			return 0
-		})
-	}
+            return 0
+        })
+    }
 
 
 }
--- test.js
+++ test.js
@@ -7,4 +7,4 @@ var Explorer = require('./components/Explorer.js')
 // *** Test ***
 
 var explorer = new Explorer()
-console.log(explorer.browse(['/Users/axa/Desktop/test']))
+console.log(explorer.browse(['/Users/axa/Desktop/test1', '/Users/axa/Desktop/test2']))
