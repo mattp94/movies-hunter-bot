5a57c91790dbbdf4ff0d7c4230fcdf26af902d8d
mattp94 <matthieu.puibaraud@isep.fr>
2016-05-03 18:02:18 +0800

    add test & bot

---
+++ test.js
@@ -0,0 +1,10 @@
+// *** Libraries declaration ***
+
+var Explorer = require('./components/Explorer.js')
+
+
+
+// *** Test ***
+
+var explorer = new Explorer()
+console.log(explorer.browse(['/Users/axa/Desktop/test']))
