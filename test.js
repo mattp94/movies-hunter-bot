6d5e9fb1cd75a7844693592082fbef94f2d87c3f
mattp94 <matthieu.puibaraud@isep.fr>
2016-05-13 17:27:43 +0800

    explorer style update

--- test.js
+++ test.js
@@ -7,6 +7,6 @@ var api = require('./lib/api.js')
 
 // *** Test ***
 
-// console.log(explorer.browse(['/Users/axa/Desktop/test1', '/Users/axa/Desktop/test2']))
+console.log(explorer.browse(['/Users/axa/Desktop/test1', '/Users/axa/Desktop/test2']))
 // console.log(api.google('les bronzés allocine', 'fr', 'fr', 5))
-console.log(api.allocine(198937))
+// console.log(api.allocine(198937))
