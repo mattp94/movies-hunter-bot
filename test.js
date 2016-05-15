ff1c4f3350c8f956dcf19fe166b0d829d8017ef3
mattp94 <matthieu.puibaraud@isep.fr>
2016-05-16 02:25:15 +0800

    extractor update (getCodes, getDate, downloadPoster)

--- test.js
+++ test.js
@@ -2,11 +2,21 @@
 
 var explorer = require('./lib/explorer.js')
 var api = require('./lib/api.js')
+var extractor = require('./lib/extractor.js')
 
 
 
 // *** Test ***
 
-console.log(explorer.browse(['/Users/axa/Desktop/test1', '/Users/axa/Desktop/test2']))
+// console.log(explorer.browse(['/Users/matthieu/Desktop/mh_tests/test1', '/Users/matthieu/Desktop/mh_tests/test2']))
 // console.log(api.google('les bronzés allocine', 'fr', 'fr', 5))
-// console.log(api.allocine(198937))
+// console.log(JSON.stringify(api.allocine(204027)))
+// console.log(extractor.allocine.getCodes('les bronzés'))
+
+var codes = extractor.allocine.getCodes('LA GLACE ET LE CIEL')
+console.log(codes)
+
+var movie = extractor.allocine.getData(codes[0])
+console.log(movie)
+
+extractor.downloadPoster(movie)
