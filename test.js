c2b15d188579610a1a3a939bb080fc2f3910766c
mattp94 <matthieu.puibaraud@isep.fr>
2016-06-06 02:37:37 +0800

    add core (NOT STABLE)

--- test.js
+++ test.js
@@ -3,20 +3,33 @@
 var explorer = require('./lib/explorer.js')
 var api = require('./lib/api.js')
 var extractor = require('./lib/extractor.js')
+var core = require('./lib/core.js')
+
+var sleep = require('thread-sleep')
 
 
 
 // *** Test ***
 
-console.log(explorer.browse(['/Users/matthieu/Desktop/mymovies/']))
+// console.log(explorer.browse(['/Users/matthieu/Desktop/mymovies/']))
 // console.log(api.google('les bronzés allocine', 'fr', 'fr', 5))
 // console.log(JSON.stringify(api.allocine(204027)))
 // console.log(extractor.allocine.getCodes('les bronzés'))
 
-var codes = extractor.allocine.getCodes('Un éléphant ça trompe énormément')
-console.log(codes)
+// var codes = extractor.allocine.getCodes('Barbecue')
+// console.log(codes)
+//
+// var movie = extractor.allocine.getData(codes[0])
+// console.log(movie)
+//
+// extractor.savePoster(movie)
 
-var movie = extractor.allocine.getData(codes[0])
-console.log(movie)
+var folders = explorer.browse(['/Users/matthieu/Desktop/mymovies/'])
 
-extractor.savePoster(movie)
+folders.forEach(function (folder) {
+    var res = core.job(folder)
+    console.log(folder.search, res)
+    sleep(2000)
+})
+
+core.done()
