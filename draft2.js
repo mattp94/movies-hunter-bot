4babba12a2e1dec1b02f4d2a5f7c57f23b953d6d
mattp94 <matthieu.puibaraud@isep.fr>
2016-10-09 23:50:59 +0200

    recursion: array instead of callback

--- draft2.js
+++ draft2.js
@@ -7,4 +7,4 @@ var utility = require('./lib/utility.js')
 
 // *** Draft 2 ***
 
-console.log(explorer.browse(['/Users/matthieu/Desktop/recursive-movies', '/Users/matthieu/Desktop/recursive-movies2']))
+console.log(explorer.browse(['/Users/matthieu/Desktop/recursive-movies']))
---
+++ reset.bash
@@ -0,0 +1,4 @@
+rm ./data/db.json
+rm ./data/posters/full/*
+rm ./data/posters/thumbnail/*
+rm ./data/trailers/*
