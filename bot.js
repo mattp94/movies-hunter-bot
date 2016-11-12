9a59cac81a9eeee9c0ae57c46a724c9d24e2beb1
mattp94 <matthieu.puibaraud@isep.fr>
2016-11-12 03:25:34 +0100

    BotError type + -.js

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 // *** Libraries ***
 
-var explorer = require('./lib/explorer.js')
-var core = require('./lib/core.js')
-var config = require('./lib/config.js')
+var explorer = require('./lib/explorer')
+var core = require('./lib/core')
+var config = require('./lib/config')
+var error = require('./lib/error')
 
 var fs = require('fs')
 
@@ -12,21 +13,28 @@ var fs = require('fs')
 
 var files = explorer.browse(config.directories, function (pathname) { // Get potential movies
     var stats = fs.statSync(pathname)
-    
+
     return (stats.isFile() || stats.isDirectory()) && config.extensions.length > 0 && new RegExp('\.(' + config.extensions.join('|') + ')$', 'i').test(pathname)
 })
 
 files.forEach(function (file) {
-    var result
+    try {
+        var result
 
-    result = core.movieHandler(file) // Add movie and from
-    result = core.duplicateHandler(result) // Add duplicate or undefined
-    result = core.posterHandler(result) // Add poster?
-    result = core.trailerHandler(result) // Add trailer?
-             core.insertionHandler(result)
-             core.sleepHandler(result)
+        result = core.movieHandler(file) // Add movie and from
+        result = core.duplicateHandler(result) // Add duplicate or undefined
+        result = core.posterHandler(result) // Add poster?
+        result = core.trailerHandler(result) // Add trailer?
+                 core.insertionHandler(result)
+                 core.sleepHandler(result)
 
-    console.log(file.search, result.from, result.poster ? 'poster' : '', result.trailer ? 'trailer' : '', result.duplicate ? 'duplicate' : '')
+        console.log(file.search, result.from, result.poster ? 'poster' : '', result.trailer ? 'trailer' : '', result.duplicate ? 'duplicate' : '')
+    } catch (e) {
+        if (e instanceof error.BotError)
+            console.log(e.message)
+        else
+            throw e
+    }
 })
 
 core.done()
