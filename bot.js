3261a9698cefccbee3c7bc9a6074ef89d86d824f
mattp94 <matthieu.puibaraud@isep.fr>
2016-11-30 01:06:59 +0100

    add logger + duplicates in log (to remove)

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var explorer = require('./lib/explorer')
 var core = require('./lib/core')
 var config = require('./lib/config')
 var error = require('./lib/error')
+var logger = require('./lib/logger')
 
 var fs = require('fs-plus')
 
@@ -11,28 +12,32 @@ var fs = require('fs-plus')
 
 // *** Bot ***
 
+logger.init()
+
 var files = explorer.browse(config.directories, function (pathname) { // Get potential movies
     return (fs.isFileSync(pathname) || fs.isDirectorySync(pathname)) && config.extensions.length > 0 && new RegExp('\.(' + config.extensions.join('|') + ')$', 'i').test(pathname)
 })
 
-files.forEach(function (file) {
+files.forEach(function (file, index) { // Scrap data for each movie
     try {
         var result
 
         result = core.movieHandler(file) // Add movie and from
         result = core.duplicateHandler(result) // Add duplicate or undefined
-        result = core.posterHandler(result) // Add poster?
-        result = core.trailerHandler(result) // Add trailer?
+        result = core.posterHandler(result) // Add poster (true or false)
+        result = core.trailerHandler(result) // Add trailer (true or false)
                  core.insertionHandler(result)
                  core.sleepHandler(result)
 
-        console.log(file.search, result.from, result.poster ? 'poster' : '', result.trailer ? 'trailer' : '', result.duplicate ? 'duplicate' : '')
+        logger.success(result, file, index, files.length)
     } catch (e) {
         if (e instanceof error.LoggedError)
-            console.log(file.search, 'error: [' + e.message + ']')
+            logger.failure(e.message, file, index, files.length)
         else
             throw e
     }
 })
 
-core.done() // Flush tmp
+core.flushHandler() // Flush tmp
+
+logger.done()
