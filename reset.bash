751f7c7aae1be25d8d8aa3e94fbb1ade8e09f6be
mattp94 <matthieu.puibaraud@isep.fr>
2016-12-07 12:27:19 +0100

    little update (reset + api comment)

--- reset.bash
+++ reset.bash
@@ -1,5 +1,8 @@
-rm ./data/db.json
-rm ./data/posters/full/*
-rm ./data/posters/thumbnail/*
-rm ./data/trailers/*
-rm ./logs/*
+# rm ./data/db.json
+# rm ./data/posters/full/*
+# rm ./data/posters/thumbnail/*
+# rm ./data/trailers/*
+# rm ./logs/*
+
+rm -r ./data
+rm -r ./logs
