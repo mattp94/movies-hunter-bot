9ea3b21d140a0f08c35aa64335a5bd89ed45b826
mattp94 <matthieu.puibaraud@isep.fr>
2017-05-21 17:23:52 +0200

    thumbnailer is async

--- reset.js
+++ reset.js
@@ -13,9 +13,9 @@ const directories = [config.locations.posters, config.locations.thumbnails, conf
 const files = [config.locations.db, config.locations.log]
 
 Promise.all([
-    ...directories.map(pathname => fs.emptyDir(utility.absolute(pathname))),
-    ...files.map(pathname => fs.remove(utility.absolute(pathname))),
-    ...directories.map(pathname => fs.remove(utility.tmp(utility.absolute(pathname), true)))
+    ...directories.map(pathname => fs.emptyDir(utility.abs(pathname))),
+    ...files.map(pathname => fs.remove(utility.abs(pathname))),
+    ...directories.map(pathname => fs.remove(utility.tmp(utility.abs(pathname), true)))
 ]).catch(err => {
     console.log(err)
 })
